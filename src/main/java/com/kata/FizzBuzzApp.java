57015269aebca87eee3fbfbfc52cf895ce771844
Sega Diop <diop.sega@econostic.net>
2019-05-07 09:15:42 +0100

    Refactoring : - Add Static import on enum Constant - Add Java 8 syntax on loops

--- src/main/java/com/kata/FizzBuzzApp.java
+++ src/main/java/com/kata/FizzBuzzApp.java
@@ -7,6 +7,6 @@ import java.util.stream.IntStream;
 public class FizzBuzzApp {
     public static void main(String[] args) {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        IntStream.range(1, 100).forEach(i -> System.out.println(fizzBuzz.convert(i)));
+        IntStream.range(1, 101).forEach(i -> System.out.println(fizzBuzz.convert(i)));
     }
 }
